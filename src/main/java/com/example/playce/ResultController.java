ed69f94cc2fb87a1bad44c44ec5e4232d4407515
Katie Mei <katiemei888@gmail.com>
2018-05-19 17:27:24 -0700

    Fixed result controller

--- src/main/java/com/example/playce/ResultController.java
+++ src/main/java/com/example/playce/ResultController.java
@@ -4,10 +4,10 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.*
 
 @RestController
 public class ResultController {
@@ -18,12 +18,11 @@ public class ResultController {
     }
 
     @RequestMapping("/testDatabase")
-    public DatabaseResult generateDatabase() {
+    public void generateDatabase() {
 
-       MysqlDataSource dataSource = new MysqlDataSource();
-       dataSource.setURL("us-cdbr-iron-east05.cleardb.net");
-       dataSource.setUser("bd9b14204c0c56");
-       dataSource.setPassword("2daf5b5d");
+       Class.forName("com.mysql.jdbc.Driver");                             
+       Connection con = DriverManager.getConnection(                       
+       "us-cdbr-iron-east-05.cleardb.net", "bd9b14204c0c56", "2daf5b5d");                
 
        Connection conn = dataSource.getConnection();
        Statement stmt = conn.createStatement();
