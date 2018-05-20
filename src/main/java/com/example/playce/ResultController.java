e897081f2be5a15c6058eafab4128bd7e259b663
Katie Mei <katiemei888@gmail.com>
2018-05-19 17:45:55 -0700

    testing ResultController with database

--- src/main/java/com/example/playce/ResultController.java
+++ src/main/java/com/example/playce/ResultController.java
@@ -18,14 +18,14 @@ public class ResultController {
     }
 
     @RequestMapping("/testDatabase")
-    public void generateResult() {
+    public Result generateResult() {
        try {
        Class.forName("com.mysql.jdbc.Driver");                             
        Connection con = DriverManager.getConnection(                       
        "us-cdbr-iron-east-05.cleardb.net", "bd9b14204c0c56", "2daf5b5d");                
        Statement stmt = con.createStatement();
        ResultSet rs = stmt.executeQuery("select * from playces where name=FirestoneGrill");
-       //return new Result(rs.getString(1), rs.getInt(2), rs.getDouble(3), rs.getString(4));
+       return new Result(rs.getString(1), rs.getInt(2), rs.getDouble(3), rs.getString(4));
        } catch (Exception e) {
          System.out.println(e);
        }
