a75cbc771bd517df50c2703da277a719b0561872
Katie Mei <katiemei888@gmail.com>
2018-05-22 14:36:50 -0700

    Adjust price request and changed commenting on column name

--- src/main/java/com/example/playce/ResultController.java
+++ src/main/java/com/example/playce/ResultController.java
@@ -44,7 +44,7 @@ public class ResultController {
        // 1: id
        // 2: name
        // 3: price
-       // 4: result
+       // 4: rating
        // 5: address
        // 6: type 
 */
@@ -63,7 +63,7 @@ public class ResultController {
           Connection con = DriverManager.getConnection(                       
           "jdbc:mysql://us-cdbr-iron-east-05.cleardb.net/heroku_3cf2d9a2c001143?reconnect=true", "bd9b14204c0c56", "2daf5b5d");                
           Statement stmt = con.createStatement();
-          String query = "select * from playces where price=\"" + questionnaireResult.getPrice() + "\" and type=\"" + questionnaireResult.getCategory() + "\" and rating>=\"" + questionnaireResult.getRating() + "\"";
+          String query = "select * from playces where price<=\"" + questionnaireResult.getPrice() + "\" and type=\"" + questionnaireResult.getCategory() + "\" and rating>=\"" + questionnaireResult.getRating() + "\"";
           ResultSet rs = stmt.executeQuery(query);
           rs.next();  
 	  return new Result(rs.getString(2), rs.getInt(3), rs.getDouble(4), rs.getString(5), rs.getString(6));
