aa87aee602cdefc9a17aff3f4f2ce824f9a22a61
Katie Mei <katiemei888@gmail.com>
2018-05-22 11:24:55 -0700

    changed escape char

--- src/main/java/com/example/playce/ResultController.java
+++ src/main/java/com/example/playce/ResultController.java
@@ -24,8 +24,7 @@ public class ResultController {
           Connection con = DriverManager.getConnection(                       
           "jdbc:mysql://us-cdbr-iron-east-05.cleardb.net/heroku_3cf2d9a2c001143?reconnect=true", "bd9b14204c0c56", "2daf5b5d");                
           Statement stmt = con.createStatement();
-          String x = "FirestoneGrill";
-          ResultSet rs = stmt.executeQuery("select * from playces where name='"+x"'");
+          ResultSet rs = stmt.executeQuery("select * from playces where name=\"FirestoneGrill\"");
           return new Result("firestone grill", 1, 1, "address is not given");
        ///return new Result(rs.getString(1), rs.getInt(2), rs.getDouble(3), rs.getString(4));
        } catch (Exception e) {
