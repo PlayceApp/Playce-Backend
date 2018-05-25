a8879bc89fa946ab8d8b1fe0bdb27a5af1058ea5
Katie Mei <katiemei888@gmail.com>
2018-05-25 00:28:25 -0700

    trying new builder technique

--- src/main/java/com/example/playce/MultipleResults.java
+++ src/main/java/com/example/playce/MultipleResults.java
@@ -4,22 +4,29 @@ import java.util.Optional;
 import lombok.Builder;
 
 // goes from shortest to longest distance
+@Builder
 public class MultipleResults {
 
     private final Result firstResult;
+/*
     private final Optional<Result> secondResult;
     private final Optional<Result> thirdResult;
     private final Optional<Result> fourthResult;
-    private final Optional<Result> fifthResult;
-
+ */ @Builder.Default private final Optional<Result> secondResult = Optional.empty();
+    @Builder.Default private final Optional<Result> thirdResult = Optional.empty();
+    @Builder.Default private final Optional<Result> fourthResult = Optional.empty();
+ 
+    @Builder.Default private final Optional<Result> fifthResult = Optional.empty();
+/*
     @Builder
     public MultipleResults(Result firstResult, Result secondResult, Result thirdResult, Result fourthResult, Result fifthResult) {
         this(firstResult, 
-            (Result) Optional.ofNullable(secondResult),
+            (Optional<Result>) Optional.ofNullable(secondResult),
             (Optional<Result>) Optional.ofNullable(thirdResult),
             (Optional<Result>) Optional.ofNullable(fourthResult),
             (Optional<Result>) Optional.ofNullable(fifthResult));
     }
+*/
 
     public Result getFirstResult() {
         return firstResult;
