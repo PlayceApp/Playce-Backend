e1824546596e5b63e0ae193fc586dc51b8d846ea
Katie Mei <katiemei888@gmail.com>
2018-05-24 21:57:11 -0700

    adding multiple results

---
+++ src/main/java/com/example/playce/MultipleResults.java
@@ -0,0 +1,39 @@
+import java.util.Optional;
+
+// goes from shortest to longest distance
+public class MultipleResults {
+
+    private final Result firstResult;
+    private final Optional<Result> secondResult;
+    private final Optional<Result> thirdResult;
+    private final Optional<Result> fourthResult;
+    private final Optional<Result> fifthResult;
+
+    public MultipleResults(Result firstResult, Result secondResult, Result thirdResult, Result fourthResult, Result fifthResult) {
+        this.firstResult = firstResult;
+        this.secondResult = Optional.of(secondResult);
+        this.thirdResult = Optional.of(thirdResult);
+        this.fourthResult = Optional.of(fourthResult);
+        this.fifthResult = Optional.of(fifthResult);
+    }
+
+    public Result getFirstResult() {
+        return firstResult;
+    }
+
+    public Result getSecondResult() {
+        return secondResult.get();
+    }
+
+    public Result getThirdResult() {
+        return thirdResult.get();
+    }
+
+    public Result getFourthResult() {
+        return fourthResult.get();
+    }
+
+    public Result getFifthResult() {
+        return fifthResult.get();
+    }
+}
