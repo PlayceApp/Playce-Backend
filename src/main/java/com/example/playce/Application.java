3dbf7a16452d7946943458351b148e64ecbd2f48
Katie Mei <katiemei888@gmail.com>
2018-05-28 15:50:06 -0700

    fixed imports' :

--- src/main/java/com/example/playce/Application.java
+++ src/main/java/com/example/playce/Application.java
@@ -3,9 +3,9 @@ package com.example.playce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servelet.config.annotation.CorsRegistry;
-import org.springframework.web.servelet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servelet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  
 
 @SpringBootApplication
