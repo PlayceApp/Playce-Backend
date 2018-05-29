2bd3115e128b2234428d17876924437c4e3e759b
spoorthyv <sckortyman@gmail.com>
2018-05-29 09:52:24 -0700

    Try printing file path

--- src/main/java/com/example/playce/JSONController.java
+++ src/main/java/com/example/playce/JSONController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.File;
 
 @RestController
 public class JSONController {
@@ -13,7 +14,16 @@ public class JSONController {
     @ResponseBody
     public String json() {
         //String contents = new String(Files.readAllBytes(Paths.get("/vast-wave-50282/src/main/resources/ValidatedQuestionnaires/recreation.json")));
-        return System.getProperty("user.dir");
+        File[] paths;
+        String s = "";
+        File f = new File("/app");
+
+        paths = f.listFiles();
+         
+        for(File path:paths) {     
+            s = s + path;
+        }
+        return s;
     }
 }
 
