9a49646d951e3ebe78bc9d9b8586b5f3290b4200
spoorthyv <sckortyman@gmail.com>
2018-05-28 17:59:43 -0700

    Try just returning string again2

--- src/main/java/com/example/playce/Questionnaire.java
+++ src/main/java/com/example/playce/Questionnaire.java
@@ -9,14 +9,6 @@ public class Questionnaire {
     private final double longitude;
     private final String cuisine;
 
-    public Questionnaire() {
-        this.price = "";
-        this.category = "";
-        this.rating = -1;
-        this.latitude = -1;
-        this.longitude = -1;
-        this.cuisine = "";
-    }
     public Questionnaire(String price, String category, double rating, double latitude, double longitude, String cuisine) {
         this.price = price;
         this.category = category;
@@ -29,42 +21,24 @@ public class Questionnaire {
     public String getPrice() {
         return price;
     }
-    public void setPrice(String price) {
-        this.price = price;
-    }
 
     public String getCategory() {
         return category;
-    }
-    public void setCategory(String category) {
-        this.category = category;
     }
 
     public double getRating() {
         return rating;
     }
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
 
     public double getLatitude() {
         return latitude;
-    }
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
     }
 
     public double getLongitude() {
         return longitude;
     }
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
 
     public String getCuisine() {
         return cuisine;
-    }
-    public void setCuisine(String cuisine) {
-        this.cuisine = cuisine;
     }
 }
