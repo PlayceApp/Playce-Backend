4daee719ea7880d2117b95b35089a00031b15b2a
spoorthyv <sckortyman@gmail.com>
2018-05-29 10:18:37 -0700

    Try returning file

--- src/main/java/com/example/playce/JSONController.java
+++ src/main/java/com/example/playce/JSONController.java
@@ -5,28 +5,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.io.IOException;
+import org.springframework.core.io.*;
+// import java.io.File;
+// import java.nio.file.Files;
+// import java.nio.file.Paths;
+// import java.io.IOException;
 
 @RestController
 public class JSONController {
 
-    @RequestMapping("/recreation")
-    @ResponseBody
-    public String json() throws IOException {
-        String contents = new String(Files.readAllBytes(Paths.get("/app/src/main/resources/ValidatedQuestionnaires/recreation.json")));
-        // File[] paths;
-        // String s = "";
-        // File f = new File("/app/src/main/resources/ValidatedQuestionnaires/recreation.json");
+    // @RequestMapping("/recreation")
+    // @ResponseBody
+    // public String json() throws IOException {
+    //     String contents = new String(Files.readAllBytes(Paths.get("/app/src/main/resources/ValidatedQuestionnaires/recreation.json")));
+    //     return contents;
+    // }
 
-        // paths = f.listFiles();
-         
-        // for(File path:paths) {     
-        //     s = s + "\n" + path;
-        // }
-        return contents;
+    @RequestMapping(value = "/app/src/main/resources/ValidatedQuestionnaires/recreation.json", method = RequestMethod.GET)
+    @ResponseBody 
+    public FileSystemResource getFile(@PathVariable("recreation.json") String fileName) {
+        return new FileSystemResource(myService.getFileFor(fileName)); 
     }
 }
+
 
