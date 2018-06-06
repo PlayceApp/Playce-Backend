8b1c8bae680b6207aa1a813fbf34137b56ecebcd
Kent Tran <ktran111@calpoly.edu>
2018-06-05 22:55:57 -0700

    Added first round of testing.

--- src/test/java/com/example/playce/KTResultControllerTest.java
+++ src/test/java/com/example/playce/KTResultControllerTest.java
@@ -7,15 +7,26 @@ import com.example.playce.Result;
 import com.example.playce.ResultController;
 
 @RunWith(SpringRunner.class)
-public class GetPlayceResultTest {
+public class KTResultControllerTest {
 
 	@Test
-	public void testGetPlayceResult() {
+	public void testGetPlayceResult1() {
 	   ResultController r = new ResultController();
 	   String playceName = "San Diego Museum Of Arts";
 	   
 	   Result expected = new Result("San Diego Museum Of Arts", 0, 0.0, 
-	       "San Luis Obispo CA 93401 USA",35.2828, -120.66, "recreation");
+	       "San Luis Obispo CA 93401 USA", 35.2828, -120.66, "recreation");
+	   
+	   Assert.assertTrue(r.generatePlayceResult(playceName).isEqualTo(expected));
+        }
+
+	@Test
+	public void testGetPlayceResult2() {
+	   ResultController r = new ResultController();
+	   String playceName = "Sidecar";
+	   
+	   Result expected = new Result("Sidecar", 0, 4.4, 
+	       "1127 Broad St San Luis Obispo CA 93401 USA", 35.2784, -120.664, "restaurant");
 	   
 	   Assert.assertTrue(r.generatePlayceResult(playceName).isEqualTo(expected));
         }
---
+++ src/test/java/com/example/playce/TVResultControllerTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.Assert;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.example.playce.Result;
+import com.example.playce.ResultController;
+
+@RunWith(SpringRunner.class)
+public class TVResultControllerTest {
+
+	@Test
+	public void testGetPlayceResult1() {
+	   ResultController r = new ResultController();
+	   String playceName = "Firestone Grill";
+	   
+	   Result expected = new Result("Firestone Grill", 1, 4.5, 
+	       "1001 Higuera St San Luis Obispo CA 93401", 35.2811, -120.66, "restaurant");
+	   
+	   Assert.assertTrue(r.generatePlayceResult(playceName).isEqualTo(expected));
+	}
+	
+	@Test
+	public void testGetPlayceResult2() {
+	   ResultController r = new ResultController();
+	   String playceName = "Chick-fil-A";
+   
+	   Result expected = new Result("Chick-fil-A", 1, 5.0, 
+	       "1 Grand Ave St San Luis Obispo CA 93407", 35.3005, -120.658, "restaurant");
+	   
+	   Assert.assertTrue(r.generatePlayceResult(playceName).isEqualTo(expected));
+	}
+}
